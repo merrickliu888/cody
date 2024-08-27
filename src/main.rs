8cb45ea4fd961375cae496f94ad2f81bdc73fb0f
Merrick Liu <merrickliu168@gmail.com>
2024-08-27 11:29:52 -0400

    todo list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    // TODO
+    // Natural Language Prompts (AI)
+    // Build in commands
+    // Command Parsing
+    // Command Execution
+    // I/O Redirection
+    // Pipes
+    // Environment Variables
     println!("Hello, world!");
 }
