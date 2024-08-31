cd303861483be86f9cb17c0a96133d78e8d38ed1
Merrick Liu <merrickliu168@gmail.com>
2024-08-31 12:23:51 -0400

    execute commands

--- src/shell/shell_commands.rs
+++ src/shell/shell_commands.rs
@@ -1,21 +1,23 @@
 use reqwest;
-use std::env;
 use std::fs::File;
 use std::path::Path;
 use std::process::{Child, Command, Output, Stdio};
+use std::{env, io};
 
 pub fn execute_input(input: &str) -> Result<CommandType, String> {
     let first_command_name = input.split_whitespace().next().unwrap();
     if first_command_name == "cody" {
-        execute_ai_command(input);
+        return execute_ai_command(input);
     } else if first_command_name == "cd" {
         return execute_cd(input);
     } else if first_command_name == "exit" {
         return Ok(CommandType::Exit);
     } else {
-        execute_commands(&mut parse_commands(input));
+        match execute_commands(&mut parse_commands(input)) {
+            Ok(command_type) => return Ok(command_type),
+            Err(err) => return Err(format!("{}", err)),
+        }
     }
-    return Ok(CommandType::Other);
 }
 
 pub fn parse_commands(input: &str) -> Vec<ShellCommand> {
@@ -35,7 +37,7 @@ pub fn parse_commands(input: &str) -> Vec<ShellCommand> {
     return commands;
 }
 
-pub fn execute_ai_command(_input: &str) {
+pub fn execute_ai_command(_input: &str) -> Result<CommandType, String> {
     unimplemented!()
     // // get prompt
     // let user_prompt: &str = input[4..].trim_start();
@@ -77,10 +79,10 @@ pub fn execute_cd(input: &str) -> Result<CommandType, String> {
     return Ok(CommandType::Other);
 }
 
-pub fn execute_commands(shell_commands: &mut Vec<ShellCommand>) {
-    let mut child_processes: Vec<Child> = Vec::new();
-
-    let mut prev_process = None;
+// Assumes everything has been parsed correctly.
+pub fn execute_commands(shell_commands: &mut Vec<ShellCommand>) -> io::Result<CommandType> {
+    let mut prev_child_process: Option<Child> = None;
+    let num_commands = shell_commands.len();
     for (
         index,
         ShellCommand {
@@ -93,52 +95,28 @@ pub fn execute_commands(shell_commands: &mut Vec<ShellCommand>) {
     {
         // Handling input redirection
         if let Some(input_file) = input_redirection {
-            let file_result = File::open(input_file);
-            match file_result {
-                Ok(file) => {
-                    command.stdin(Stdio::from(file));
-                }
-                Err(err) => eprintln!("{}", err),
-            }
-        } else if index > 0 {
-            let prev_command = &child_processes[index - 1];
-            command.stdin(Stdio::from(prev_command.stdout.unwrap()));
+            let file = File::open(input_file)?;
+            command.stdin(Stdio::from(file));
+        } else if let Some(prev) = prev_child_process {
+            command.stdin(Stdio::from(prev.stdout.unwrap()));
         }
 
         // Handling output redirection
         if let Some(output_file) = output_redirection {
-            let file_result = File::options().create(true).append(*append).open(output_file);
-            match file_result {
-                Ok(file) => {
-                    command.stdout(Stdio::from(file));
-                }
-                Err(err) => eprintln!("{}", err),
-            }
+            let file = File::options().create(true).append(*append).open(output_file)?;
+            command.stdout(Stdio::from(file));
+        } else if index < num_commands - 1 {
+            command.stdout(Stdio::piped());
         }
 
         // Executing commands
-        let command_spawn = command.spawn();
-        match command_spawn {
-            Ok(child) => {
-                child_processes.push(child);
-                prev_process = Some(&child);
-            }
-            Err(err) => eprintln!("{}", err),
-        }
+        prev_child_process = Some(command.spawn()?);
     }
 
-    // Collection exit statuses (possibly change and use wait_with_output, or wait with id)
-    // TODO wait with pid, collect vector of pid, and keep track of prev child to get stdin and out
-    for child in child_processes.iter_mut() {
-        let child_status = child.wait();
-        if let Ok(exit_status) = child_status {
-            if !exit_status.success() {
-                eprintln!("Command failed")
-            }
-        } else if let Err(err) = child_status {
-            eprintln!("{}", err);
-        }
-    }
+    // Waiting on last child
+    let mut last_command = prev_child_process.unwrap();
+    last_command.wait()?;
+    return Ok(CommandType::Other);
 }
 
 pub struct ShellCommand {
