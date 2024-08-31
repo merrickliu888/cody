d83c7adf4fb272b18b2efabd6b19ddb489fb9743
Merrick Liu <merrickliu168@gmail.com>
2024-08-30 21:31:56 -0400

    main shell function

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,19 @@
-#![allow(unused)]
-
 mod shell {
     pub mod shell_commands;
     pub mod variables;
 }
+use shell::shell_commands;
 use shell::shell_commands::*;
 use shell::variables::*;
 
 use std::collections::HashMap;
 use std::env;
 use std::io::{self, Write};
-use std::process::{Child, Command, Stdio};
+use std::process::{Command, Stdio};
 
 fn main() {
-    // TODO
-    // Natural Language Prompts (AI) = limit use of ai functionality based on shell functionality
-    // Build in commands
-    // Command Parsing
-    // Command Execution
-    // I/O Redirection
-    // Pipes
-    // Environment Variables
-
     // Initialize the shell
-    // let (_ollama_server, _ollama_llm) = init_shell();
+    let (_ollama_server, _ollama_llm) = init_shell();
     let mut shell_variables: HashMap<String, String> = HashMap::new();
     loop {
         // Print prompt
@@ -38,6 +28,7 @@ fn main() {
         if input == "" {
             continue;
         }
+
         // Parse variables here.
         let input_with_variables_inserted: String = match insert_variables(input, &shell_variables) {
             Ok(new_input) => new_input,
@@ -47,27 +38,22 @@ fn main() {
             }
         };
 
-        // Checking if we are setting a variable
+        // Checking if we are setting a variable else executing commands
         if input_with_variables_inserted.contains("=") {
-            if let Err(err) = handle_variable_assigment(&input, &mut shell_variables) {
-                eprintln!("Failed to assign variable: {}", err)
-            } else {
-                println!("Variable assigned!");
+            if let Err(err) = handle_variable_assigment(&input_with_variables_inserted, &mut shell_variables) {
+                eprintln!("Failed to assign variable: {}", err);
             }
-            continue;
+        } else {
+            let execution_result = execute_input(&input_with_variables_inserted);
+            match execution_result {
+                Ok(command_type) => {
+                    if let shell_commands::CommandType::Exit = command_type {
+                        break;
+                    }
+                }
+                Err(err) => eprintln!("Failed to execute commands: {}", err),
+            }
         }
-
-        // Parse commands
-        // let commands: Vec<ShellCommand> =
-        //     ShellCommand::parse_commands(&input, &shell_variables).expect("Failed to parse commands.");
-        // if BUILT_IN_SHELL_COMMANDS.contains(commands[0].name) {
-        //     ShellCommand::handle_built_in_shell_commands(&commands).expect("Failed to run built in commands.");
-        // // TODO how to handle exit?
-        // } else {
-        //     ShellCommand::handle_shell_commands(&commands).expect("Failed to run commands.");
-        // }
-
-        println!("{}", input_with_variables_inserted);
     }
 }
 
@@ -84,12 +70,14 @@ impl Drop for ChildGuard {
 fn init_shell() -> (ChildGuard, ChildGuard) {
     let ollama_server = Command::new("ollama")
         .arg("serve")
-        // .stderr(Stdio::null())
+        .stdin(Stdio::null())
+        .stderr(Stdio::null())
         .spawn()
         .expect("Failed to initialize Cody: ollama serve");
     let ollama_llm = Command::new("ollama")
         .args(["run", "llama3.1"])
-        // .stderr(Stdio::null())
+        .stdin(Stdio::null())
+        .stderr(Stdio::null())
         .spawn()
         .expect("Failed to initialize Cody: ollama run llama3.1");
     println!("Cody initialized with LLama3.1");
