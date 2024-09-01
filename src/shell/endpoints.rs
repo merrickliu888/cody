feb501296ac6edbd41f5267bb573a2366f381f94
Merrick Liu <merrickliu168@gmail.com>
2024-09-01 00:24:04 -0400

    local

--- src/shell/endpoints.rs
+++ src/shell/endpoints.rs
@@ -35,27 +35,27 @@ pub struct GenerateResponseLocal {
 }
 
 pub fn generate_response_local(prompt: String) -> Result<String, String> {
-    // Send prompt to Ollama
-    // let url = "http://localhost:11434/api/generate";
-    // let client = reqwest::blocking::Client::new();
-    // let mut body = GenerateBody::default();
-    // body.prompt = user_prompt.to_string();
-    // let res = client.post(url).json(&body).send();
+    let url = "http://localhost:11434/api/generate";
+    let client = reqwest::blocking::Client::new();
+    let body = GenerateBodyLocal {
+        prompt,
+        ..Default::default()
+    };
+    let auth_token = format!("Bearer: {}", COHERE_API_KEY.as_str());
 
-    // // Handle Response
-    // if let Err(err) = res {
-    //     return Err(format!("Failed to send request to Ollama: {}", err));
-    // }
-    // let res = res.unwrap();
-    // if !res.status().is_success() {
-    //     return Err(format!("Failed to get response from Ollama: {}", res.status()));
-    // }
-    // let res = res.json()::<GenerateResponse>();
+    let res = client
+        .post(url)
+        .header(AUTHORIZATION, auth_token)
+        .json(&body)
+        .send()
+        .map_err(|err| err.to_string())?;
+    if !res.status().is_success() {
+        return Err(format!("Request Failed: {}", res.status()));
+    }
 
-    // // execute commands
+    let llm_generated_shell_commands = res.json::<GenerateResponseLocal>().unwrap().response;
 
-    // unimplemented!()
-    unimplemented!()
+    return Ok(llm_generated_shell_commands);
 }
 
 #[derive(Serialize)]
